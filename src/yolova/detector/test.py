58aa72de01c76fcdf9c123acb9ff342dd8ad45f3
mr-aydemir <kaandemircan97@gmail.com>
2022-07-02 21:34:00 +0300

    Initial commit

---
+++ src/yolova/detector/test.py
@@ -0,0 +1,21 @@
+if __name__ == "__main__":
+    from sahi.model import Yolov5DetectionModel
+    from sahi.utils.cv import read_image
+    from sahi.predict import get_prediction, get_sliced_prediction, predict
+    from IPython.display import Image
+    from sahi.utils.yolov5 import (
+        download_yolov5s6_model,
+    )
+    import torchvision
+
+    for_detect = ".\test\2_35_frame_000788.jpg"
+    detection_model = Yolov5DetectionModel(
+        model_path="D:\Teknofest\YOLOVA\ConnectionInterface\TAKIM_BAGLANTI_ARAYUZU\src\yolova\models\Best3_Ekleme9.pt",
+        image_size=640,
+        confidence_threshold=0.5,
+        device="cuda:0" # "cpu",
+    )
+    
+    result = get_prediction(for_detect, detection_model)
+    result.export_visuals(export_dir="demo_data/")
+    Image("demo_data/prediction_visual.png")
